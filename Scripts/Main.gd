bf4b0d8c9c9f9b75e7513a6802beb7ffa568eb52
Jack Kinsey <jack_kinsey@sweetwater.com>
2020-03-14 23:11:17 -0500

    add HUD

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,26 +5,26 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
-
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(0)
+	$HUD.show_message("Get Ready!")
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
-	
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
@@ -42,3 +42,5 @@ func _on_MobTimer_timeout():
 	# Set the velocity (speed & direction).
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
+	$HUD.connect("start_game", mob, "_on_start_game")
--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -7,6 +7,8 @@ var mob_types = ["walk", "swim", "fly"]
 func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
-
 func _on_Visibility_screen_exited():
+	queue_free()
+	
+func _on_start_game():
 	queue_free()
